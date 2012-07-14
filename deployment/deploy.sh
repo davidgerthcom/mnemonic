bd998b27919314f499fcbd46627acc90d90170d1
David Gerth <git@davidgerth.com>
2012-07-14 14:59:00 +0200

    functional update

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,11 +1,11 @@
 echo "Removing current mnemonic directory..."
-ssh david@davidgerth.com rm -rf /var/www/mnemonic
+ssh david@192.168.h2.103 rm -rf /var/www/mnemonic
 
 echo "Cloning git repository into davidgerth.com/var/www/mnemonic..."
 ssh david@davidgerth.com git clone https://github.com/davidgerthcom/mnemonic.git /var/www/mnemonic
 
-echo "Creating config dir..."
-ssh david@davidgerth.com mkdir /var/www/mnemonic/application/configs
+#echo "Creating config dir..."
+#ssh david@davidgerth.com mkdir /var/www/mnemonic/application/configs
 
-echo "Creating application.ini"
-scp ../application/configs/application.ini david@davidgerth.com:/var/www/mnemonic/application/configs/application.ini
+#echo "Creating application.ini"
+#scp ../application/configs/application.ini david@davidgerth.com:/var/www/mnemonic/application/configs/application.ini
